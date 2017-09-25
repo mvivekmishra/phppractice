55625f667f9d20d13b1789a5ffb32d93a54ff2a7
vivek mishra <vm368@afsconnect1.njit.edu>
2017-09-24 23:18:52 -0400

    With All 10 Function

--- array.php
+++ array.php
@@ -1,4 +1,5 @@
 <?php
+echo ' araay concepts'.'<br>';
 $value=array(
 1,
 2,
@@ -6,27 +7,35 @@ $value=array(
 4
 );
 $value[]=5;
+echo 'Display Method '.'<br>';
 print_r($value);
-//$value=(int) array_values($value);
+echo '<hr>';
+echo 'Function UNSET'.'<br>';
 unset($value[2]);
 echo '<br>';
 print_r($value);
-//echo $value;
 echo '<hr>';
-$name=array('vivek','mishra','ucid-vm368');
-echo "first name: $name[0]";
-echo '<br>';
-echo "first name: {$name[0]}";
+echo 'FUNCTION FOREACH'.'<br>';
+foreach($value as $ele)
+echo $ele;
+echo '<hr>';
+echo 'Associative Array' .'<br>';
+$name=array('vivek' => 601,'mishra' => 603,'ucid-vm368' => 661);
+echo 'first name value:'. $name['vivek'].'<br>';
+echo '<hr>';
+echo 'Function count '. '<br>';
+echo 'no of elements:'. count($name).'<br>';
 echo '<br>';
 print_r($name);
-unset($name[1]);
 echo '<br>';
-print_r($name);
+//print_r($name);
 echo '<hr>';
+echo 'Function Array_Values '.'<br>';
 echo 'for udate index value';
 $name=array_values($name);
 print_r($name);
 echo '<hr>';
+echo 'rand function '.'<br>';
 $numbers=array();
 for ($i=0;$i<5;$i++)
 {
@@ -41,6 +50,13 @@ $number_string= array('a','b','c');
 $k=count($number_string);
 echo $k;
 echo '<br>';
+echo 'Filled Function :' .'<br>';
+$number_count_new= array_fill(0,6,2);
+for ($m=0;$m<count($number_count_new);$m++)
+{
+	echo 'Filled array with 2: ' . $number_count_new[$m].'<br>';
+}
+echo '<br>';
 print_r($number_string);
 echo '<hr>';
 $prices=array(10,20,30,40,50);
@@ -50,12 +66,20 @@ for ($k=0; $k<count($prices);$k++)
 	$sum += $prices[$k];
 }
 echo $sum;
+echo '<hr>';
+echo 'Merge Function'.'<br>';
+$new_array= array_merge($prices, $number_string);
+print_r($new_array);
 echo '<br>';
 $avg= $sum / count($prices);
 echo $avg;
 echo '<br>';
 foreach($prices as $z);
 echo $z;
+echo '<hr>';
+echo ' Reverse Function :'.'<br>';
+$reverse_array= array_reverse($new_array);
+print_r($reverse_array);
 
 
 
